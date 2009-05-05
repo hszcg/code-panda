4f021c6949b1dbc9ae7eac2e389c26c6c00b837c
zhangcg1989@gmail.com <zhangcg1989@gmail.com@035f06f6-041c-11de-ada4-5153b8187bf2>
2009-05-05 08:14:35 +0000

    +Modify 代码框架更新，解释抽象工厂和代码动态加载

---
+++ trunk/Code-Pandas/src/org/codepanda/application/CommandVisitorFactory.java
@@ -0,0 +1,16 @@
+package org.codepanda.application;
+
+/**
+ * @author hszcg
+ *
+ */
+public class CommandVisitorFactory {
+	/**
+	 * @param commandType
+	 * @param commandDetail
+	 * @return
+	 */
+	public static CommandVisitor creator( CommandType commandType, String commandDetail){
+		return null;
+	}
+}
