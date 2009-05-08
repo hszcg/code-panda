3af9b2b0755377cb81851660676079c460e3492d
hmlee9 <hmlee9@035f06f6-041c-11de-ada4-5153b8187bf2>
2009-05-08 16:15:30 +0000

    // tonigh!

--- Code-Pandas/src/org/codepanda/application/factory/UserActorFactory.java
+++ Code-Pandas/src/org/codepanda/application/factory/UserActorFactory.java
@@ -1,5 +1,13 @@
 package org.codepanda.application.factory;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import org.codepanda.utility.contact.*;
+import javax.xml.parsers.*;
 
+import org.w3c.dom.*;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 import org.codepanda.application.CommandActor;
 import org.codepanda.application.CommandType;
 import org.codepanda.application.user.NewUserActor;
@@ -10,20 +18,181 @@ import org.codepanda.utility.user.User;
  *
  */
 public class UserActorFactory extends CommandActorFactory {
-
+	 User currentUser;
 	@Override
 	public CommandActor creator(CommandType commandType, String commandDetail) {
 		// TODO Auto-generated method stub
-		
-		// 仅作为样例
+		commandType=CommandType.NEW_USER;
+		commandDetail=
+			"<com>"+
+			"<NewUser>"+"</NewUser>"+
+			"<UserName>"+"leilei"+"</UserName>"+
+			"<Telephone>"+"13699252256"+"</Telephone>"+
+			"<Telephone>"+"51531174"+"</Telephone>"+
+			"</com>";
 		if( commandType == CommandType.NEW_USER ){
-			NewUserActor newUserActor = new NewUserActor();
-			//从commandDetail中解析出用户信息
-			User currentUser = new User();
-			
+			NewUserActor newUserActor = new NewUserActor();			
+			currentUser = new User();
 			newUserActor.setNewUser(currentUser);
+		//从commandDetail中解析出用户信息
+		try
+		{
+			DocumentBuilderFactory dbf=DocumentBuilderFactory.newInstance();
+			DocumentBuilder db=dbf.newDocumentBuilder();
+			//Document document=db.parse(commandDetail);
+			Document document=db.parse(new InputSource(new StringReader(commandDetail)));
+			
+			//System.out.println("File Path"+document.getDocumentURI());
+			Element root=document.getDocumentElement();
+			Iterator(root);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return newUserActor;
 		}
 		return null;
 	}
+	public  void Iterator(Element element)
+	{
+		NodeList nodelist=element.getChildNodes();
+	//	System.out.println(element.getNodeName());
+		for(int i=0;i<nodelist.getLength();i++)
+		{
+			System.out.println(i);
+			Node node=nodelist.item(i);
+			String str=node.getNodeName();
+			/* if(i==0)
+			{
+				if(str.equalsIgnoreCase("NewUser"))
+				{
+					System.out.println("New User!!!");
+					continue;
+				}
+				else
+				{
+					System.out.println("Wrong Function!!!");
+					break;
+				}
+			}*/
+			//else
+			//{
+			if(str.equalsIgnoreCase("UserName"))
+			{
+				String value=node.getTextContent();
+				currentUser.setUserName(value);
+				System.out.println("UserName--"+value);
+			}
+			else if(str.equalsIgnoreCase("UserPassword"))
+			{
+				String value=node.getTextContent();
+				currentUser.setPassword(value);
+				System.out.println("UserPassword--"+value);
+			}
+			else if(str.equalsIgnoreCase("Telephone"))
+			{
+				String value=node.getTextContent();
+				System.out.println(value);
+				ArrayList<String> myphoneNumberList=currentUser.getPhoneNumberList();
+				if(myphoneNumberList==null)
+				{
+					myphoneNumberList=new ArrayList<String>();
+				}
+				myphoneNumberList.add(value);
+				currentUser.setPhoneNumberList(myphoneNumberList);				
+				System.out.println("Telephone--"+value);
+			}
+			else if(str.equalsIgnoreCase("Email"))
+			{
+				String value=node.getTextContent();
+				ArrayList<String> myEmailList=currentUser.getEmailAddresseList();
+				if(myEmailList==null)
+				{
+					myEmailList=new ArrayList<String>();
+				}
+			
+				myEmailList.add(value);
+				currentUser.setEmailAddresseList(myEmailList);
+				System.out.println("Email--"+value);
+			}
+			else if(str.equalsIgnoreCase("Address"))
+			{
+				String value=node.getTextContent();
+				ArrayList<String> myAddressList=currentUser.getContactAddressList();
+				if(myAddressList==null)
+				{
+					myAddressList=new ArrayList<String>();
+				}
+			
+				myAddressList.add(value);
+				currentUser.setContactAddressList(myAddressList);
+				System.out.println("Address--"+value);
+			}
+			else if(str.equalsIgnoreCase("Office"))
+			{
+				String value=node.getTextContent();
+				ArrayList<String> myOfficeList=currentUser.getWorkingDepartmentList();
+				if(myOfficeList==null)
+				{
+					myOfficeList=new ArrayList<String>();
+				}
+			
+				myOfficeList.add(value);
+				currentUser.setWorkingDepartmentList(myOfficeList);
+				System.out.println("Office--"+value);
+			}
+			else if(str.equalsIgnoreCase("IMContact"))
+			{
+				String value=node.getTextContent();
+				ArrayList<String> myimContactList=currentUser.getImContactInformationList();
+				if(myimContactList==null)
+				{
+					myimContactList=new ArrayList<String>();
+				}
+			
+				myimContactList.add(value);
+				currentUser.setImContactInformationList(myimContactList);
+				System.out.println("IMContact--"+value);
+			}
+			else if(str.equalsIgnoreCase("Birthday"))
+			{
+				String value=node.getTextContent();
+				String str1[]= new String[3];
+				str1=value.split(" ");
+				Birthday  birth=new Birthday();
+				birth.setYear(Integer.parseInt(str1[0]));
+				birth.setMonth(Integer.parseInt(str1[1]));
+				birth.setDay(Integer.parseInt(str1[2]));
+				currentUser.setContactBirthday(birth);
+				System.out.println("Birthday--"+value);
+			}
+			else if(str.equalsIgnoreCase("url"))
+			{
+				String value=node.getTextContent();
+				ArrayList<String> myurlList=currentUser.getUrlList();
+				if(myurlList==null)
+				{
+					myurlList=new ArrayList<String>();
+				}			
+				myurlList.add(value);
+				currentUser.setUrlList(myurlList);
+				System.out.println("Url--"+value);
+			}
+			if(node instanceof Element)
+			{
+				//System.out.println("asdf");
+				Iterator((Element)node);
+			}
+			
+		//	} 
+		}
+	}
+/*	public static void main(String args[])
+	{
+		UserActorFactory  uac=new UserActorFactory();
+		uac.creator(CommandType.NEW_USER,"<com>"+"<NewUser>"+"</NewUser>"+"<UserName>"+"leilei"+"</UserName>"+"<Telephone>"+"13699252256"+"</Telephone>"+"<Telephone>"+"51531174"+"</Telephone>"+"</com>");
+		
+	}*/
 
 }
--- Code-Pandas/src/org/codepanda/utility/user/User.java
+++ Code-Pandas/src/org/codepanda/utility/user/User.java
@@ -7,13 +7,10 @@ import org.codepanda.utility.contact.ContactData;
 import org.codepanda.utility.contact.ContactOperations;
 import org.codepanda.utility.contact.HeadImage;
 import org.codepanda.utility.label.RelationLabel;
-
 public class User implements UserOperations,ContactOperations {
-	private ContactData userContactData;
-	
+	private ContactData userContactData=new ContactData();
 	private String userName;
 	private String password;
-
 	@Override
 	public String getUserName() {
 		// TODO Auto-generated method stub
@@ -23,6 +20,11 @@ public class User implements UserOperations,ContactOperations {
 	@Override
 	public int varifyUserPassword(String inputPassword) {
 		// TODO Auto-generated method stub
+		//验证用户输入密码是否正确，正确返回1，错误返回0
+		if(this.getPassword().equalsIgnoreCase(inputPassword))
+		{
+			return 1;
+		}
 		return 0;
 	}
 
@@ -49,159 +51,166 @@ public class User implements UserOperations,ContactOperations {
 
 	@Override
 	public ArrayList<String> getCommonLabelList() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return userContactData.commonLabelList;
 	}
 
 	@Override
 	public ArrayList<String> getContactAddressList() {
 		// TODO Auto-generated method stub
-		return null;
+		return userContactData.contactAddressList;
 	}
 
 	@Override
 	public Birthday getContactBirthday() {
 		// TODO Auto-generated method stub
-		return null;
+		return userContactData.contactBirthday;
 	}
 
 	@Override
 	public String getContactName() {
 		// TODO Auto-generated method stub
-		return null;
+		return userContactData.contactName;
 	}
 
 	@Override
 	public ArrayList<String> getEmailAddresseList() {
 		// TODO Auto-generated method stub
-		return null;
+		return userContactData.emailAddresseList;
 	}
 
 	@Override
 	public ArrayList<String> getGroupList() {
 		// TODO Auto-generated method stub
-		return null;
+		return userContactData.groupList;
 	}
 
 	@Override
 	public HeadImage getHeadImage() {
 		// TODO Auto-generated method stub
-		return null;
+		return userContactData.headImage;
 	}
 
 	@Override
 	public String getISN() {
 		// TODO Auto-generated method stub
-		return null;
+		return userContactData.ISN;
 	}
 
 	@Override
 	public ArrayList<String> getImContactInformationList() {
 		// TODO Auto-generated method stub
-		return null;
+		return userContactData.imContactInformationList;
 	}
 
 	@Override
 	public ArrayList<String> getPhoneNumberList() {
 		// TODO Auto-generated method stub
-		return null;
+		return userContactData.phoneNumberList;
 	}
 
 	@Override
 	public ArrayList<RelationLabel> getRelationLabelList() {
 		// TODO Auto-generated method stub
-		return null;
+		return userContactData.relationLabelList;
 	}
 
 	@Override
 	public ArrayList<String> getUrlList() {
 		// TODO Auto-generated method stub
-		return null;
+		return userContactData.urlList;
 	}
 
 	@Override
 	public ArrayList<String> getWorkingDepartmentList() {
 		// TODO Auto-generated method stub
-		return null;
+		return userContactData.workingDepartmentList;
 	}
 
 	@Override
 	public void setCommonLabelList(ArrayList<String> commonLabelList) {
 		// TODO Auto-generated method stub
-		
+		userContactData.commonLabelList=commonLabelList;	   
 	}
 
 	@Override
 	public void setContactAddressList(ArrayList<String> contactAddressList) {
 		// TODO Auto-generated method stub
-		
+		userContactData.contactAddressList=contactAddressList;
 	}
 
 	@Override
 	public void setContactBirthday(Birthday contactBirthday) {
 		// TODO Auto-generated method stub
-		
+		userContactData.contactBirthday=contactBirthday;
 	}
 
 	@Override
 	public void setContactName(String contactName) {
 		// TODO Auto-generated method stub
-		
+		userContactData.contactName=contactName;
 	}
 
 	@Override
 	public void setEmailAddresseList(ArrayList<String> emailAddresseList) {
 		// TODO Auto-generated method stub
-		
+		userContactData.emailAddresseList=emailAddresseList;
 	}
 
 	@Override
 	public void setGroupList(ArrayList<String> groupList) {
 		// TODO Auto-generated method stub
-		
+		userContactData.groupList=groupList;
 	}
 
 	@Override
 	public void setHeadImage(HeadImage headImage) {
 		// TODO Auto-generated method stub
-		
+		userContactData.headImage=headImage;
 	}
 
 	@Override
 	public void setISN(String isn) {
 		// TODO Auto-generated method stub
-		
+		userContactData.ISN=isn;
 	}
 
 	@Override
 	public void setImContactInformationList(
 			ArrayList<String> imContactInformationList) {
 		// TODO Auto-generated method stub
-		
+		userContactData.imContactInformationList=imContactInformationList;
 	}
 
 	@Override
 	public void setPhoneNumberList(ArrayList<String> phoneNumberList) {
 		// TODO Auto-generated method stub
-		
+		userContactData.phoneNumberList=phoneNumberList;
 	}
 
 	@Override
 	public void setRelationLabelList(ArrayList<RelationLabel> relationLabelList) {
 		// TODO Auto-generated method stub
-		
+		userContactData.relationLabelList=relationLabelList;
 	}
 
 	@Override
 	public void setUrlList(ArrayList<String> urlList) {
 		// TODO Auto-generated method stub
-		
+		userContactData.urlList=urlList;
 	}
 
 	@Override
 	public void setWorkingDepartmentList(ArrayList<String> workingDepartmentList) {
 		// TODO Auto-generated method stub
+		userContactData.workingDepartmentList=workingDepartmentList;
+	}
+	
+	public void createNewUser(User UserToBeAdded)
+	{
+		UserToBeAdded.password=this.getPassword();
+		UserToBeAdded.userName=this.getUserName();
+		UserToBeAdded.userContactData=this.userContactData;
 		
 	}
-
 }
