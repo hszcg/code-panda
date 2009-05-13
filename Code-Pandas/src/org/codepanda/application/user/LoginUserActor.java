5a2a00fe65136f1c803dc3bc11f0258de8d859c3
eva19870729@gmail.com <eva19870729@gmail.com@035f06f6-041c-11de-ada4-5153b8187bf2>
2009-05-13 15:03:35 +0000

    Add LoginUser

--- Code-Pandas/src/org/codepanda/application/user/LoginUserActor.java
+++ Code-Pandas/src/org/codepanda/application/user/LoginUserActor.java
@@ -1,13 +1,29 @@
 package org.codepanda.application.user;
 
 import org.codepanda.application.CommandActor;
+import org.codepanda.utility.user.User;
 
 public class LoginUserActor implements CommandActor {
-
+		private User  user=null;
+		
 	@Override
 	public int executeCommand() {
 		// TODO Auto-generated method stub
+		if(this.user==null)
+		{
+			System.out.println("User Null!!!");
+		}
 		return 0;
 	}
+	public void setUser(User user) {
+		this.user = user;
+	}
 
+
+	/**
+	 * @return the newUser
+	 */
+	public User getNewUser() {
+		return user;
+	}
 }
