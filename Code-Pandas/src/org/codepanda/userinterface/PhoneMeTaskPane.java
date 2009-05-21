f74ce05fffbb44c6c03cd549df5b60ee3a944de0
zhangcg1989@gmail.com <zhangcg1989@gmail.com@035f06f6-041c-11de-ada4-5153b8187bf2>
2009-05-21 19:07:01 +0000

    +Modify	TaskPane

--- Code-Pandas/src/org/codepanda/userinterface/PhoneMeTaskPane.java
+++ Code-Pandas/src/org/codepanda/userinterface/PhoneMeTaskPane.java
@@ -8,90 +8,85 @@ import javax.swing.tree.*;
 
 import org.jdesktop.swingx.*;
 
-public class PhoneMeTaskPane extends JXTaskPaneContainer
-								implements TreeSelectionListener {
-	//JXTaskPane currentUserPane;
-	//JXLabel usernameLabel;
-	//JTextField usernameField;
-	
+public class PhoneMeTaskPane extends JXTaskPaneContainer implements
+		TreeSelectionListener {
+	// JXTaskPane currentUserPane;
+	// JXLabel usernameLabel;
+	// JTextField usernameField;
+
 	private PhoneMeFrame mainFrame;
-	private JXTaskPane details;
+	private JXTaskPane searchTaskPane;
 	private JTextField searchField;
 
-	//JXTaskPane contactListPane;
-	JScrollPane treeView;
-	JTree contactListTree;
-	
-	public PhoneMeTaskPane(PhoneMeFrame phoneMeFrame){
-		super();
-		/*currentUserPane = new JXTaskPane();
-		currentUserPane.setTitle("用户信息");
-		usernameLabel = new JXLabel("当前用户");
-		usernameField = new JTextField("");
-		currentUserPane.add(usernameLabel);
-		currentUserPane.add(usernameField);
-		add(currentUserPane);*/
-		// create another taskPane, it will show details of the selected file
-		this.mainFrame = phoneMeFrame;
-		details = new JXTaskPane();
-		details.setTitle("按姓名搜索    ");
+	private JXTaskPane contactListTaskPane;
+	private JScrollPane treeViewPane;
+	private JTree contactListTree;
 
-		// add standard components to the details taskPane
-		//JXLabel searchLabel = new JXLabel("Search:");
+	public PhoneMeTaskPane(PhoneMeFrame phoneMeFrame) {
+		super();
+		this.mainFrame = phoneMeFrame;
+
+		// 搜索
+		searchTaskPane = new JXTaskPane();
+		searchTaskPane.setTitle("按姓名搜索    ");
 		searchField = new JTextField("");
-		//details.add(searchLabel);
-		details.add(searchField);
-		add(details);
-		
+		searchTaskPane.add(searchField);
+		this.add(searchTaskPane);
+
 		configureContactList();
 	}
-	
+
+	public void configureContactList() {
+		DefaultMutableTreeNode root = new DefaultMutableTreeNode("所有分组");
+		createTree(root);
+		contactListTree = new JTree(root);
+		contactListTree.getSelectionModel().setSelectionMode(
+				TreeSelectionModel.SINGLE_TREE_SELECTION);
+		contactListTree.addTreeSelectionListener(this);
+		treeViewPane = new JScrollPane(contactListTree);
+
+		contactListTaskPane = new JXTaskPane();
+		contactListTaskPane.setTitle("联系人分组列表 ");
+		contactListTaskPane.add(treeViewPane);
+		this.add(contactListTaskPane);
+	}
+
+	public void valueChanged(TreeSelectionEvent e) {
+		DefaultMutableTreeNode node = (DefaultMutableTreeNode) contactListTree
+				.getLastSelectedPathComponent();
+		if (node == null)
+			// Nothing is selected.
+			return;
+		Object nodeInfo = node.getUserObject();
+		if (node.isLeaf()) {
+			System.out.println(((TreeNodeItem) nodeInfo).getISN());
+		} else {
+			// displayURL(helpURL);
+		}
+	}
+
+	/**
+	 * @param root
+	 */
 	private void createTree(DefaultMutableTreeNode root) {
 		DefaultMutableTreeNode group = null;
 		DefaultMutableTreeNode contact = null;
-		
+
 		group = new DefaultMutableTreeNode("同学");
 		root.add(group);
-		
+
 		contact = new DefaultMutableTreeNode(new TreeNodeItem("张三", 1));
 		group.add(contact);
 		contact = new DefaultMutableTreeNode(new TreeNodeItem("李四", 2));
 		group.add(contact);
-		
+
 		group = new DefaultMutableTreeNode(new TreeNodeItem("朋友", 3));
 		root.add(group);
-		
+
 		contact = new DefaultMutableTreeNode(new TreeNodeItem("王五", 4));
 		group.add(contact);
 		contact = new DefaultMutableTreeNode(new TreeNodeItem("赵六", 5));
 		group.add(contact);
-	}
-	
-	public void configureContactList(){
-		DefaultMutableTreeNode root =
-            new DefaultMutableTreeNode("联系人分组列表");
-		createTree(root);
-		contactListTree = new JTree(root);
-		contactListTree.getSelectionModel().setSelectionMode
-        (TreeSelectionModel.SINGLE_TREE_SELECTION);
-		contactListTree.addTreeSelectionListener(this);
-		treeView = new JScrollPane(contactListTree);
-		add(treeView);
-	}
-	
-	public void valueChanged(TreeSelectionEvent e) {
-		DefaultMutableTreeNode node = (DefaultMutableTreeNode)
-        contactListTree.getLastSelectedPathComponent();
-		if (node == null)
-			//Nothing is selected.	
-			return;
-		Object nodeInfo = node.getUserObject();
-		if (node.isLeaf()) {
-			System.out.println(((TreeNodeItem)nodeInfo).getISN());
-		} 
-		else {
-			//displayURL(helpURL);
-		}
 	}
 
 	/**
@@ -101,22 +96,26 @@ public class PhoneMeTaskPane extends JXTaskPaneContainer
 		return mainFrame;
 	}
 
-	private class TreeNodeItem{
+	/**
+	 * @author hszcg
+	 * 
+	 */
+	private class TreeNodeItem {
 		private String name;
 		private Integer iSN;
 
-		TreeNodeItem(String name, Integer iSN){
+		TreeNodeItem(String name, Integer iSN) {
 			this.name = name;
 			this.iSN = iSN;
 		}
-		
-		public String toString(){
+
+		public String toString() {
 			return name;
 		}
-		
+
 		public Integer getISN() {
 			return iSN;
 		}
-		
+
 	}
 }
