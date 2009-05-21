9f45d58e0d0df9d7bd48ce7c158d961cb74f5792
zhangcg1989@gmail.com <zhangcg1989@gmail.com@035f06f6-041c-11de-ada4-5153b8187bf2>
2009-05-21 01:09:24 +0000

    +Modify LoginUser 修改返回逻辑判断

--- Code-Pandas/src/org/codepanda/userinterface/messagehandler/LoginUserMessageHandler.java
+++ Code-Pandas/src/org/codepanda/userinterface/messagehandler/LoginUserMessageHandler.java
@@ -1,13 +1,23 @@
 package org.codepanda.userinterface.messagehandler;
 
 import org.codepanda.application.CommandVisitor;
+import org.codepanda.application.user.LoginUserActor;
 import org.codepanda.userinterface.utility.LoginResultType;
 
+/**
+ * @author hszcg
+ *
+ */
 public class LoginUserMessageHandler implements MessageHandler {
 	@Override
 	public Object executeCommand(CommandVisitor commandVistor) {
-		commandVistor.getCommandActor().executeCommand();
+		int result = commandVistor.getCommandActor().executeCommand();
 		
+		if(result == LoginUserActor.USERNAME_NOT_EXIST)
+			return LoginResultType.USERNAME_NOT_EXIST;
+		else if(result == LoginUserActor.INVAILD_PASSWORD)
+			return LoginResultType.INVAILD_PASSWORD;
+			
 		return LoginResultType.SUCCEED;
 	}
 
