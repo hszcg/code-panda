cd78304766bf3f142d33a3498131e3b06c9a2f6e
zhangcg1989@gmail.com <zhangcg1989@gmail.com@035f06f6-041c-11de-ada4-5153b8187bf2>
2009-06-21 20:25:15 +0000

    +Add About Information

--- trunk/Code-Pandas/src/org/codepanda/userinterface/PhoneMeAboutUsDialog.java
+++ trunk/Code-Pandas/src/org/codepanda/userinterface/PhoneMeAboutUsDialog.java
@@ -5,34 +5,59 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
-public class PhoneMeAboutUsDialog extends JDialog{
-	JTextArea info;
-	JButton confirm;
-	JPanel southPanel;
-	public PhoneMeAboutUsDialog(PhoneMeFrame mainFrame, 
-			String title, boolean mark){
+public class PhoneMeAboutUsDialog extends JDialog {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4431678750159996379L;
+
+	private JLabel picShow;
+	private JTextArea info;
+	private JPanel centerPanel;
+	private JButton confirm;
+	private JPanel southPanel;
+
+	public PhoneMeAboutUsDialog(PhoneMeFrame mainFrame, String title,
+			boolean mark) {
 		super(mainFrame, title, mark);
-		
+
 		setLayout(new BorderLayout());
-		
-		//TODO add logo at north
-		
+
+		// TODO add logo at north
+		centerPanel = new JPanel();
+
+		try {
+			picShow = new JLabel();
+			ImageIcon p = new ImageIcon(ImageIO.read(this.getClass().getResource("/icon/Panda.gif")));
+			picShow.setIcon(p);
+			centerPanel.add(picShow, BorderLayout.EAST);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
 		info = new JTextArea();
-		info.setText("主要开发人员：\n" +
-				"许丹青\t\t张晨光\n" +
-				"李慧岷\t\t张昕宇\n\n" +
-				"完成时间：2009年6月22日\n" +
-				"Google code网址：\n" +
-				"http://code.google.com/p/code-panda/ \n");
+		info.setText("Code Pandas!\n" + "Developers：\n" + "许丹青\tleilei\n"
+				+ "李慧岷\tAN\n" + "张昕宇\tZhangxy\n" + "张晨光\thszcg\n\n"
+				+ "Build v2009.06.22\n\n" + "Project Website：\n"
+				+ "http://code.google.com/p/code-panda/ \n");
 		info.setEditable(false);
-		add(info,"Center");
-		
+		centerPanel.add(info, BorderLayout.CENTER);
+		add(centerPanel, BorderLayout.CENTER);
+
+		//
 		southPanel = new JPanel();
 		confirm = new JButton("确定");
 		confirm.addActionListener(new ActionListener() {
@@ -41,14 +66,19 @@ public class PhoneMeAboutUsDialog extends JDialog{
 			}
 		});
 		southPanel.add(confirm);
-		add(southPanel, "South");
-		
+		add(southPanel, BorderLayout.SOUTH);
+
 		pack();
-		Dimension screenSize = Toolkit.getDefaultToolkit()
-				.getScreenSize();
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		setLocation((screenSize.width - getWidth()) / 2,
 				(screenSize.height - getHeight()) / 2);
 		setResizable(false);
 		setVisible(true);
+
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				dispose();
+			}
+		});
 	}
 }
